ede1e7b0cfe92301e702ffaad2336ac40de2bd74
Víctor <arenas.v86@gmail.com>
2016-11-26 21:38:46 +0100

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var webserver = require('gulp-webserver');
+
+gulp.task('sass', function() {
+  gulp.src('./src/spidercss.scss')
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('css', function() {
+  gulp.src('./dist/spidercss.css')
+    .pipe(cleanCSS())
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('serve', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      port: 2021,
+      livereload: true
+    }));
+});
+
+gulp.task('default', ['sass', 'css', 'serve'], function() {
+  gulp.watch(['./src/*.scss', './src/**/*.scss'], ['sass', 'css']);
+});
